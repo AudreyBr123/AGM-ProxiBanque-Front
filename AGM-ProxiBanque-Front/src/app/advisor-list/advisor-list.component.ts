e7007c91470fafb3aae66c3875402ea9bcc8624b
gwendal <51480637+gwendal25@users.noreply.github.com>
2024-05-08 09:32:12 +0200

    AP-24 feat ✨: added base components
    Added advisor-list, client-list, client-create, client-show, client-edit, footer, header, login pages

---
+++ AGM-ProxiBanque-Front/src/app/advisor-list/advisor-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advisor-list',
+  templateUrl: './advisor-list.component.html',
+  styleUrl: './advisor-list.component.css'
+})
+export class AdvisorListComponent {
+
+}
---
+++ AGM-ProxiBanque-Front/src/app/client-edit/client-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-edit',
+  templateUrl: './client-edit.component.html',
+  styleUrl: './client-edit.component.css'
+})
+export class ClientEditComponent {
+
+}
