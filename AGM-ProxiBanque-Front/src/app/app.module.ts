e3fc7504c3af968e869a4f58071799c84104dc7f
AudreyBr123 <boureau.audrey@gmail.com>
2024-05-08 15:44:15 +0200

    AP-12-chore :sparkles: created simple login page

--- AGM-ProxiBanque-Front/src/app/app.module.ts
+++ AGM-ProxiBanque-Front/src/app/app.module.ts
@@ -17,6 +17,7 @@ import {MatTableModule} from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -38,7 +39,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormField,
+    MatFormFieldModule
   ],
   providers: [
     provideAnimationsAsync()
