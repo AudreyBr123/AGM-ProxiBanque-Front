ae021ef951d867c566b49dea73b1a143529cc93d
AudreyBr123 <boureau.audrey@gmail.com>
2024-05-20 10:45:53 +0200

    AP-23-feat :sparkles: get client by id from MYSQL added

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,30 +1,54 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ClientModel } from '../models/client.model';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
-
+  
   endpoint = 'http://localhost:8080/clients'
   constructor(private httpClient : HttpClient) {} //Attention à l'ajouter aussi dans app.modules.ts
-
+  
+  // httpOptions = {
+  //   headers: new HttpHeaders({
+  //     'Content-Type': 'application/json',
+  //     'Access-Control-Allow-Origin' : '*'
+  //   })
+  // }
+  
   getClients() {
     return this.httpClient.get<ClientModel[]>(this.endpoint)
   }
-
-  getClientById(clientId: number) {
-    return this.httpClient.get<ClientModel>(this.endpoint + clientId)
-  }
-
-  // EXEMPLE BASIQUE pour méthode postClient avec uniquement un attribut "firstName"
-  // A reprendre et compléter pour l'adapter au formulaire
   
-  // postClient(firstName: string) {
-  //   return this.httpClient.post<ClientModel>(this.endpoint, { firstName: firstName })
-  //   .subscribe(data => {
-  //      firstName = data.firstName }
-  //   )
-  // }
+  getClientById(clientId: number) {
+    return this.httpClient.get<ClientModel>(this.endpoint + "/" + clientId)
+    // .pipe(
+    //   catchError(this.handleError)
+    // )
+  }
+  
+//   handleError(error:any) {
+//     let errorMessage = '';
+//     if (error.error instanceof ErrorEvent) {
+//         errorMessage = error.error.message;
+//     } else {
+//         errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+//     }
+//     window.alert(errorMessage);
+//     return throwError(() => new Error(errorMessage));
+// }
+
+
+
+// EXEMPLE BASIQUE pour méthode postClient avec uniquement un attribut "firstName"
+// A reprendre et compléter pour l'adapter au formulaire
+
+// postClient(firstName: string) {
+//   return this.httpClient.post<ClientModel>(this.endpoint, { firstName: firstName })
+//   .subscribe(data => {
+//      firstName = data.firstName }
+//   )
+// }
 }
