bbfdb30549135291f048062adb9e258c73d447b0
gwendal <51480637+gwendal25@users.noreply.github.com>
2024-05-24 13:53:58 +0200

    feat ✨ : update client form

--- src/app/client-create/client-create.component.ts
+++ src/app/client-create/client-create.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ClientModel } from '../models/client.model';
-import { AddressModel } from '../models/address.model';
 import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import { PersonInfos } from '../models/person-infos';
 import { AccountModel } from '../models/account.model';
 import { ClientService } from '../services/client.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -21,10 +21,12 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrl: './client-create.component.css'
 })
 export class ClientCreateComponent {
-  personInfosModel = new PersonInfos("", "", "", "", "", "", "");
+  personInfos = new PersonInfos("", "", "", "", "", "", "");
   account = new AccountModel(null, 500.0, new Date());
-  clientModel = new ClientModel(null, this.personInfosModel, this.account);
-  isAddMode: boolean = true;
+  client = new ClientModel(0, this.personInfos, this.account);
+  id: number = 0;
+  isAddMode: boolean = false;
+  loading = false;
   submitted = false;
 
   registerForm = new FormGroup({
@@ -44,15 +46,28 @@ export class ClientCreateComponent {
     savingAccount: false
   });
 
-  constructor(private _formBuilder: FormBuilder, private service: ClientService) {
-  }
+  constructor(private _formBuilder: FormBuilder, private service: ClientService, private route: ActivatedRoute, private router: Router) {}
   
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.isAddMode = !this.id;
+
+    if(!this.isAddMode) {
+      this.service.getClientById(this.id).subscribe((DBclient) => {
+        this.client = DBclient;
+        this.personInfos = DBclient.personInfos;
+      });
+    }
+  }
+
   onSubmit() {
-    this.submitted = false;
+    this.submitted = true;
 
     if(this.registerForm.invalid) {
       return;
     }
+
+    this.loading = true;
 
     if(this.isAddMode) {
       this.createClient();
@@ -63,18 +78,39 @@ export class ClientCreateComponent {
   }
 
   resetClient() {
-    this.personInfosModel = new PersonInfos("", "", "", "", "", "", "");
+    this.personInfos = new PersonInfos("", "", "", "", "", "", "");
     this.account = new AccountModel(0, 0.0, new Date());
-    this.clientModel = new ClientModel(0, this.personInfosModel, this.account);
+    this.client = new ClientModel(0, this.personInfos, this.account);
   }
 
   private createClient() {
-    console.log(this.clientModel);
+    console.log(this.client);
     console.log(`current account : ${this.accounts.value.currentAccount}`);
-    this.service.postClient(this.clientModel).subscribe()
+
+    this.service.postClient(this.client)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/client-list']);
+        },
+        error: error => {
+          console.log("Error creating client");
+          this.loading = false;
+        }
+      })
   }
 
   private updateClient() {
-    console.log(this.clientModel);
+    console.log(this.client);
+
+    this.service.putClient(this.client.id, this.client)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/client-list'])
+        },
+        error: error => {
+          console.log("Error updating client");
+          this.loading = false;
+        }
+      })
   }
 }
--- src/app/models/client.model.ts
+++ src/app/models/client.model.ts
@@ -3,11 +3,11 @@ import { PersonInfos } from "./person-infos";
 
 
 export class ClientModel {
-    id: number | null;
+    id: number;
     personInfos: PersonInfos;
     account?: AccountModel;
 
-    constructor(id: number | null, personInfos: PersonInfos, account: AccountModel){
+    constructor(id: number, personInfos: PersonInfos, account: AccountModel){
         this.id = id;
         this.personInfos = personInfos;
         this.account = account || null;
