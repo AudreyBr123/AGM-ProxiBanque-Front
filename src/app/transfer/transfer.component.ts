4d27e46818de768708fc359bfd02151b16d3f483
AudreyBr123 <boureau.audrey@gmail.com>
2024-06-05 21:45:00 +0200

    AP-25-feat :recycle: refactoring TS code, removed variables not used

--- src/app/transfer/transfer.component.ts
+++ src/app/transfer/transfer.component.ts
@@ -17,27 +17,23 @@ import { TransferDtoRequest } from '../models/transfer-dto-request.model';
 })
 export class TransferComponent implements OnInit {
   
-  //tri à faire dans les variables
   clients: ClientModel[] = [];
-  currentAccounts: CurrentAccountModel[] = [];
-  savingAccounts: SavingAccountModel[] = [];
-  debitAccounts: AccountModel[] = [];
-  creditAccounts: AccountModel[] = [];
-
-
-  transferDtoRequest = new TransferDtoRequest("", "", 0,0,0);
-  typeCreditAccount = "";
-  typeDebitAccount = "";
-  
-  currentAccount = new CurrentAccountModel(null, 0.0, new Date());
-  savingAccount = new SavingAccountModel(null, 0.0, new Date());
   personInfos = new PersonInfos("", "", "", "", "", "", "");
-  debitClient= new ClientModel(0, this.personInfos, null, null);
-  creditClient= new ClientModel(0, this.personInfos, null, null);
-  selectedDebitAccount = new AccountModel(0, 0.0, new Date());
-  selectedCreditAccount = new AccountModel(0, 0.0, new Date());
 
-  // Comptes trouvés, à utiliser pour renvoyer le compte retrouvé avec type de compte et id client
+  debitClient= new ClientModel(0, this.personInfos, null, null);
+  debitAccounts: AccountModel[] = [];
+  typeDebitAccount = "";
+  selectedDebitAccount = new AccountModel(0, 0.0, new Date());
+  
+  creditClient= new ClientModel(0, this.personInfos, null, null);
+  creditAccounts: AccountModel[] = [];
+  typeCreditAccount = "";
+  selectedCreditAccount = new AccountModel(0, 0.0, new Date());
+  
+  transferDtoRequest = new TransferDtoRequest("", "", 0,0,0);
+  
+  
+  // Comptes trouvés, utilisé pour renvoyer le compte retrouvé avec l'id client et le compte selectionné
   debitCurrentAccount = new CurrentAccountModel(null, 0.0, new Date());
   debitSavingAccount = new SavingAccountModel(null, 0.0, new Date());
   creditCurrentAccount = new CurrentAccountModel(null, 0.0, new Date());
@@ -61,15 +57,15 @@ export class TransferComponent implements OnInit {
       this.clients = clientsFromJsonServer
     })
     
-    this.transferService.getCurrentAccounts()
-    .subscribe((acc) => {
-      this.currentAccounts = acc;
-    })  
+    // this.transferService.getCurrentAccounts()
+    // .subscribe((acc) => {
+    //   this.currentAccounts = acc;
+    // })  
     
-    this.transferService.getSavingAccounts()
-    .subscribe((acc) => {
-      this.savingAccounts = acc;
-    }) 
+    // this.transferService.getSavingAccounts()
+    // .subscribe((acc) => {
+    //   this.savingAccounts = acc;
+    // }) 
   }  
   
   
@@ -80,16 +76,18 @@ export class TransferComponent implements OnInit {
       this.debitClient = client
       this.debitCurrentAccount =  this.debitClient.currentAccount!
       this.debitSavingAccount = this.debitClient.savingAccount!
+      // Utilisé pour afficher les comptes dans le HTML
       this.debitAccounts = [this.debitCurrentAccount, this.debitSavingAccount]
     })    
   }
-
+  
   findClientAccountsCredit(idClientDebit: number) {  
     this.clientService.getClientById(idClientDebit)
     .subscribe((client) => {
       this.creditClient = client
       this.creditCurrentAccount =  this.creditClient.currentAccount!
       this.creditSavingAccount = this.creditClient.savingAccount!
+      // Utilisé pour afficher les comptes dans le HTML
       this.creditAccounts = [this.creditCurrentAccount, this.creditSavingAccount]
     })   
   }
@@ -103,44 +101,38 @@ export class TransferComponent implements OnInit {
   // Associe le type de compte à sa version String JSON, et sélectionne le bon compte
   findAccountDebit(accountType: String) {
     console.log( "type : "+ accountType);
-
+    
     if (accountType == "Compte Courant"){
       this.typeDebitAccount= "currentAccount"
       console.log("type debit account dans findAccountDebit" + this.typeDebitAccount);
       this.selectedDebitAccount = this.debitCurrentAccount  
-
+      
     } else {
       this.typeDebitAccount= "savingAccount"
       console.log("type debit account dans findAccountDebit" + this.typeDebitAccount)
       this.selectedDebitAccount = this.debitSavingAccount
-
+      
     }
     this.debitAccounts = [this.selectedDebitAccount];
   }
-
-    findAccountCredit(accountType: String) {
-      console.log( "type : "+ accountType);
-
-      if (accountType == "Compte Courant"){
-        this.typeCreditAccount= "currentAccount";
-        this.selectedCreditAccount = this.creditCurrentAccount; 
-        console.log("type credit account dans findAccountCredit" + this.typeCreditAccount);
-        
-      } else {
-        this.typeCreditAccount= "savingAccount";
-        this.selectedCreditAccount = this.creditSavingAccount;
-        console.log("type credit account dans findAccountCredit" + this.typeCreditAccount);
-      }
-      this.creditAccounts = [this.selectedCreditAccount];
+  
+  findAccountCredit(accountType: String) {
+    console.log( "type : "+ accountType);
+    
+    if (accountType == "Compte Courant"){
+      this.typeCreditAccount= "currentAccount";
+      this.selectedCreditAccount = this.creditCurrentAccount; 
+      console.log("type credit account dans findAccountCredit" + this.typeCreditAccount);
+      
+    } else {
+      this.typeCreditAccount= "savingAccount";
+      this.selectedCreditAccount = this.creditSavingAccount;
+      console.log("type credit account dans findAccountCredit" + this.typeCreditAccount);
     }
+    this.creditAccounts = [this.selectedCreditAccount];
+  }
   
   onSubmit(value: any) {
-  
-    console.log("type credit account "+this.typeCreditAccount);
-    console.log("type debit account "+this.typeDebitAccount);
-    console.log("value.typeCreditAccountFormControl  "+ value.typeCreditAccountFormControl);
-    console.log("value.typeDebitAccountFormControl " + value.typeDebitAccountFormControl);
-
     
     this.transferDtoRequest = {
       typeCreditAccount: this.typeCreditAccount, 
